219136b031a10bdda58275ec70bddc237890c92e
player4dead <player4dead@proton.me>
2026-07-15 18:45:33 +0200

    update hardware-configuration.nix

--- modules/host/raspi/hardware-configuration.nix
+++ modules/host/raspi/hardware-configuration.nix
@@ -1,27 +1,25 @@
 {
-  flake.modules.nixos.host_raspi =
-    {
-      config,
-      lib,
-      pkgs,
-      modulePath,
-      ...
-    }:
-    {
-      # imports = [
-      #   (modulePath + "/installer/scan/not-detected.nix")
-      # ];
+  flake.modules.nixos.host_raspi = {
+    config,
+    lib,
+    pkgs,
+    modulePath,
+    ...
+  }: {
+    # imports = [
+    #   (modulePath + "/installer/scan/not-detected.nix")
+    # ];
 
-      boot = {
-        supportedFilesystems = [ "zfs" ];
-        kernelModules = [ ];
-        extraModulePackages = [ ];
-        initrd = {
-          availableKernelModules = [ ];
-          kernelModules = [ ];
-        };
+    boot = {
+      supportedFilesystems = ["zfs"];
+      kernelModules = [];
+      extraModulePackages = [];
+      initrd = {
+        availableKernelModules = ["nvme" "xhci_pci" "ums_realtek" "usb_storage" "sd_mod"];
+        kernelModules = [];
       };
-
-      nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
     };
+
+    nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
+  };
 }
